dcbb4ad9158b88d7639be3c8ae44c7cc85d4837f
JINKA RANGA BHARATH <bharathjinka09@gmail.com>
2021-12-02 17:34:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+var $TABLE = $('#table');
+var $BTN = $('#export-btn');
+var $EXPORT = $('#export');
+
+$('.table-add').click(function() {
+  var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+  $TABLE.find('table').append($clone);
+});
+
+$('.table-remove').click(function() {
+  $(this).parents('tr').detach();
+});
+
+$('.table-up').click(function() {
+  var $row = $(this).parents('tr');
+  if ($row.index() === 1) return; // Don't go above the header
+  $row.prev().before($row.get(0));
+});
+
+$('.table-down').click(function() {
+  var $row = $(this).parents('tr');
+  $row.next().after($row.get(0));
+});
+
+$('.bharath').blur(function() {
+  console.log("jjjj")
+  var bha = $(this).text();
+  console.log(bha);
+  alert(bha)
+});
+
+// A few jQuery helpers for exporting only
+jQuery.fn.pop = [].pop;
+jQuery.fn.shift = [].shift;
+
+$BTN.click(function() {
+  var $rows = $TABLE.find('tr:not(:hidden)');
+  var headers = [];
+  var data = [];
+
+  // Get the headers (add special header logic here)
+  $($rows.shift()).find('th:not(:empty):not([data-attr-ignore])').each(function() {
+    headers.push($(this).text().toLowerCase());
+  });
+
+  // Turn all existing rows into a loopable array
+  $rows.each(function() {
+    var $td = $(this).find('td');
+    var h = {};
+
+    // Use the headers from earlier to name our hash keys
+    headers.forEach(function(header, i) {
+      h[header] = $td.eq(i).text(); // will adapt for inputs if text is empty
+    });
+
+    data.push(h);
+  });
+
+  // Output the result
+  $EXPORT.text(JSON.stringify(data));
+});
